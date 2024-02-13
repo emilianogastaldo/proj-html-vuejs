d0552609143ff4382501f2fa9b260042891599c5
leonardoolia <leonardooliaa@gmail.com>
2024-02-13 10:33:05 +0100

    created sponsor links and shop link components

---
+++ src/components/main/ShopLink.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+    name: 'ShopLink'
+}
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/main/SponsorLinks.vue
@@ -0,0 +1,27 @@
+<script>
+import ShopCards from './ShopCards.vue';
+// importare dati da data 
+import sponsors from '../../data/_sponsorImages'
+export default {
+    name: 'SponsorLinks',
+    data: () => ({
+        sponsors
+    }),
+    components: { ShopCards }
+
+}
+</script>
+
+<template>
+    <section id="sponsor" class="container">
+        <div class="card-container d-flex justify-content-center align-items-center column-gap-3 text-center">
+            <ShopCards v-for="sponsor in sponsors" :key="sponsor.id" v-bind="sponsor" />
+        </div>
+    </section>
+</template>
+
+<style scoped>
+section #sponsor {
+    min-height: 800px;
+}
+</style>
