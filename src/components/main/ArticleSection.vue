f659012abb579c6d70a14240e57eda4f8800c6e2
emilianogastaldo <emiliano0303@gmail.com>
2024-02-13 09:44:13 +0100

    added style to article section

--- src/components/main/ArticleSection.vue
+++ src/components/main/ArticleSection.vue
@@ -9,23 +9,35 @@ export default {
         <div class="container">
             <div class="row">
                 <div class="col-6">
-                    <h5>Latest articles</h5>
+                    <h4 class="title">Latest articles</h4>
                 </div>
                 <div class="col-6">
                     <div class="text-end">
-                        Read all articles <i class="fas fa-angle-right"></i>
+                        <a class="title" href="#">Read all articles <i class="fas fa-angle-right"></i></a>
                     </div>
                 </div>
                 <div class="col-12">
-                    <ul class="d-flex">
+                    <ul class="d-flex gap-5 mt-4">
                         <li>
-                            <img class="img-fluid" src="../../assets/images/blog1-2x-1200x773.jpg" alt="">
+                            <a href="#"><img class="img-fluid" src="../../assets/images/blog1-2x-1200x773.jpg" alt=""></a>
+                            <div class="mt-3 ms-3">
+                                <h5><a href="#" class="title">The best protein shake</a></h5>
+                                <p>By adim | Published On: November 26th, 2019 | Gym</p>
+                            </div>
                         </li>
                         <li>
-                            <img class="img-fluid" src="../../assets/images/blog1-2x-1200x773.jpg" alt="">
+                            <a href="#"><img class="img-fluid" src="../../assets/images/blog1-2x-1200x773.jpg" alt=""></a>
+                            <div class="mt-3 ms-3">
+                                <h5><a href="#" class="title">The best protein shake</a></h5>
+                                <p>By adim | Published On: November 26th, 2019 | Gym</p>
+                            </div>
                         </li>
                         <li>
-                            <img class="img-fluid" src="../../assets/images/blog1-2x-1200x773.jpg" alt="">
+                            <a href="#"><img class="img-fluid" src="../../assets/images/blog1-2x-1200x773.jpg" alt=""></a>
+                            <div class="mt-3 ms-3">
+                                <h5><a href="#" class="title">The best protein shake</a></h5>
+                                <p>By adim | Published On: November 26th, 2019 | Gym</p>
+                            </div>
                         </li>
                     </ul>
                 </div>
@@ -34,4 +46,25 @@ export default {
     </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.title {
+    font-weight: 600;
+}
+
+a {
+    text-decoration: none;
+    color: black;
+}
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+
+    li {
+        display: flex;
+        flex-direction: column;
+    }
+
+}
+</style>
