2b62cae2d2c3f43481a1d3485f8f869f5ef2d87c
emilianogastaldo <emiliano0303@gmail.com>
2024-02-13 14:42:12 +0100

    added style

--- src/components/main/YoutubeChannel.vue
+++ src/components/main/YoutubeChannel.vue
@@ -13,17 +13,19 @@ export default {
         </div>
         <div class="col-6">
             <div class="text px-5">
-                <p>Tune up your works</p>
-                <h1>The best health & fitness advice online</h1>
-                <div class="youtube-avatar d-flex">
+                <p class="color-blue up-title">Tune up your works</p>
+                <h2>The best health & fitness advice online</h2>
+                <div class="youtube-avatar d-flex my-4 gap-1">
                     <img class="img-fluid" src="../../assets/images/unnamed.jpg" alt="channel avatar">
-                    <div>
+                    <div class="channel-button">
                         <p class="m-0">ThemeFusion</p>
-                        <button>Youtube</button>
-                        <button>999+</button>
+                        <button><i class="fa-brands fa-youtube"></i> Youtube</button>
+                        <span> 999+</span>
                     </div>
                 </div>
-                <p>Arcu eu facilisi ut quisque placerat nunc habitant. Magna semper mauris, venenatis, leo integer sit
+                <p class="paragraph color-gray">Arcu eu facilisi ut quisque placerat nunc habitant. Magna semper mauris,
+                    venenatis, leo
+                    integer sit
                     pellentesque.</p>
                 <button>Visit my YouTube channel</button>
             </div>
@@ -33,6 +35,14 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+.color-blue {
+    color: #4154ff;
+}
+
+.color-gray {
+    color: #98939F;
+}
+
 .image-section {
     height: 300px;
     position: absolute;
@@ -48,5 +58,33 @@ export default {
     padding-top: 100px;
     padding-bottom: 400px;
 
+    h2 {
+        font-size: 62px;
+        font-weight: 600;
+    }
+
+    .up-title {
+        font-size: 28px;
+    }
+
+    .paragraph {
+        font-size: 23px;
+    }
+
+    .youtube-avatar {
+        font-size: small;
+
+        button {
+            color: white;
+            background-color: red;
+            border: none;
+        }
+
+        span {
+            border: 1px solid gray;
+            display: inline-block;
+            padding: 0 2px;
+        }
+    }
 }
 </style>
