29316319c4719d249b25b20aaf685e333a986b99
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-02-13 13:20:37 +0100

    added v-for and props

--- src/components/main/SubscribePlan.vue
+++ src/components/main/SubscribePlan.vue
@@ -1,9 +1,13 @@
 <script>
 import SubscribePlanCard from './SubscribePlanCard.vue';
+import { subPlan } from '../../data/index';
 export default {
     name: 'SubscribePlan',
+    data: () => ({
+        subPlan
+    }),
     components: {
-        SubscribePlanCard
+        SubscribePlanCard,
     }
 }
 </script>
@@ -11,8 +15,11 @@ export default {
 <template>
     <section id="subscribe-plan">
         <div class="container d-flex align-item-center justify-content-center">
-            <div class="row g-3 row-col-3">
-                <SubscribePlanCard />
+            <div class="row g-5 row-col-3">
+                <div class="col" v-for="card in subPlan" :key="card.id">
+                    <SubscribePlanCard :icon="card.icon" :title="card.title" :subtitle="card.subtitle"
+                        :paragrph="card.paragrph" :link="card.link" :url="card.url" />
+                </div>
             </div>
         </div>
     </section>
