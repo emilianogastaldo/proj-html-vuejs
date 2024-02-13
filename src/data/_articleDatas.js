610e984c4f1b27b7d6c44ad05d63bcc840afc203
emilianogastaldo <emiliano0303@gmail.com>
2024-02-13 09:55:15 +0100

    created article datas

---
+++ src/data/_articleDatas.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        title: 'The Best protein shake',
+        creator: 'admin',
+        date: 'November 26th, 2019',
+        subject: 'Gym',
+        img: 'blog4-2x.jpg'
+    },
+    {
+        id: 2,
+        title: 'Ultimate cardio workout',
+        creator: 'admin',
+        date: 'November 26th, 2019',
+        subject: 'Gym',
+        img: 'blog1-2x.jpg'
+    },
+    {
+        id: 3,
+        title: 'New juices available now',
+        creator: 'admin',
+        date: 'November 26th, 2019',
+        subject: 'Gym',
+        img: 'blog3-2x.jpg'
+    }
+]
