5eab7246475932622496f8fab691d5e4f00cbad1
Claudio9616 <clares9611@gmail.com>
2024-02-13 17:21:31 +0100

    add completed youtube section

--- src/components/main/YoutubePlaylist.vue
+++ src/components/main/YoutubePlaylist.vue
@@ -12,6 +12,13 @@ export default {
 </script>
 <template>
     <figure class="container my-5">
+        <div class="row justify-content-between h3 mb-5">
+            <div class="col">Featured playlists</div>
+            <div class="col text-end">
+                <a href="">View all videos
+                    <i class="fa-solid fa-angle-right"></i></a>
+            </div>
+        </div>
         <ul class="d-flex gap-5 text-center">
             <li v-for="youtubeLink in youtubeLinks" :key="youtubeLink.id">
                 <YoutubeCard v-bind="youtubeLink" />
@@ -24,5 +31,15 @@ export default {
 
 ul {
     list-style-type: none;
+    padding-left: 0;
+}
+
+a {
+    text-decoration: none;
+    color: black;
+}
+
+.row {
+    font-weight: 600;
 }
 </style>
