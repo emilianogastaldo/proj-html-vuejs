7498a5fb18f9380118e84d1dac5280e1c632dcf9
leonardoolia <leonardooliaa@gmail.com>
2024-02-13 17:30:38 +0100

    added  data in index

--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,6 @@
 import headerLinks from './_headerLinks';
 import jumbrotronImage from './_jumbrotronImage';
+import shopData from './_shopData';
+import sponsorImages from './_sponsorImages'
 
-export { headerLinks, jumbrotronImage };
+export { headerLinks, jumbrotronImage, shopData, sponsorImages };
