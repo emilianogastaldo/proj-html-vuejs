2356e23cdd0f5d6b7b5fafe68aa66d008bb35a0c
emilianogastaldo <emiliano0303@gmail.com>
2024-02-13 09:28:35 +0100

    added articles list hard-coded

--- src/components/main/ArticleSection.vue
+++ src/components/main/ArticleSection.vue
@@ -4,6 +4,34 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <section id="lastest-articles">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <h5>Latest articles</h5>
+                </div>
+                <div class="col-6">
+                    <div class="text-end">
+                        Read all articles <i class="fas fa-angle-right"></i>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <ul class="d-flex">
+                        <li>
+                            <img class="img-fluid" src="../../assets/images/blog1-2x-1200x773.jpg" alt="">
+                        </li>
+                        <li>
+                            <img class="img-fluid" src="../../assets/images/blog1-2x-1200x773.jpg" alt="">
+                        </li>
+                        <li>
+                            <img class="img-fluid" src="../../assets/images/blog1-2x-1200x773.jpg" alt="">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
 
 <style lang="scss" scoped></style>
