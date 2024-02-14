5bafe91fc1bc820d76912186f041ffccd94146d9
emilianogastaldo <emiliano0303@gmail.com>
2024-02-14 13:00:46 +0100

    created PlayButton component

---
+++ src/components/PlayButton.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'PlayButton'
+}
+</script>
+
+<template>
+    <a href="#" class="play-button">
+        <i class="fas fa-play"></i>
+    </a>
+</template>
+
+<style lang="scss" scoped>
+.play-button {
+    display: inline-block;
+    line-height: 80px;
+    text-align: center;
+    height: 80px;
+    width: 80px;
+    color: blue;
+    background-color: white;
+    border-radius: 50%;
+    font-size: 23px;
+}
+
+.play-button:hover {
+    color: #FF414B;
+}
+</style>
